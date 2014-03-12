88cddab167d5b04b719d13bac8f529eeea936a20
Hack Reactor Students <github@hackreactor.com>
2014-03-12 16:51:49 -0700

    Created recursive clone object function

---
+++ cloneObject/cloneObject.js
@@ -0,0 +1,31 @@
+var cloneObject = function(obj){
+  var cloneObj = {};
+  for(var key in obj){
+    var type;
+    if(Array.isArray(obj[key])){
+      type = 'array';
+    } else if(obj[key] === null){
+      type = 'null';
+    }
+    else{
+      type = typeof obj[key];
+    }
+    if(type === 'null'){
+      cloneObj[key] = null;
+    }
+    else if(type !== 'object'){
+      cloneObj[key] = obj[key];
+    }
+    else{
+      cloneObj[key] = cloneObject(obj[key]);
+    }
+  }
+  return cloneObj;
+};
+
+var obj = {1:[{1: {2:'hello obj'}},'b','c'], 2: true, 3: null};
+
+var obj2 = cloneObject(obj);
+
+console.log(obj);
+console.log(obj2);
