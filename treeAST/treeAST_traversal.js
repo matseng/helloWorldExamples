25314603a4c5064d6de373b65b94ea0116eef652
Michael Tseng <matseng@gmail.com>
2014-04-04 14:16:03 -0700

    Created blank treeAST file

---
+++ treeAST/treeAST_traversal.js
@@ -0,0 +1,95 @@
+/**
+  *
+  * EXAMPLE:
+var root1 = new Tree(1);
+var branch2 = root1.addChild(2);
+var branch3 = root1.addChild(3);
+var leaf4 = branch2.addChild(4);
+var leaf5 = branch2.addChild(5);
+var leaf6 = branch3.addChild(6);
+var leaf7 = branch3.addChild(7);
+var result = root1.astTraverse();
+console.log(result);  // Should print: [1, 2, 4, 2, 5, 2, 1, 3, 6, 3, 7, 3, 1]
+  *
+  */
+
+
+var Tree = function(value){
+  this.value = value;
+  this.children = [];
+  //OK TO MODIFY
+};
+
+Tree.prototype.astTraverse = function() {
+  //TODO:
+
+};
+
+/**
+ * You shouldn't need to change anything below here, but feel free to look.
+  */
+
+/**
+  * add an immediate child
+  * (wrap values in Tree nodes if they're not already)
+  */
+Tree.prototype.addChild = function(child){
+  if (!child || !(child instanceof Tree)){
+    child = new Tree(child);
+  }
+
+  if(!this.isDescendant(child)){
+    this.children.push(child);
+  } else {
+    throw new Error("That child is already a child of this tree");
+  }
+  // return the new child node for convenience
+  return child;
+};
+
+/**
+  * check to see if the provided tree is already a child of this
+  * tree __or any of its sub trees__
+  */
+Tree.prototype.isDescendant = function(child){
+  if(this.children.indexOf(child) !== -1){
+    // `child` is an immediate child of this tree
+    return true;
+  }else{
+    for(var i = 0; i < this.children.length; i++){
+      if(this.children[i].isDescendant(child)){
+        // `child` is descendant of this tree
+        return true;
+      }
+    }
+    return false;
+  }
+};
+
+/**
+  * remove an immediate child
+  */
+Tree.prototype.removeChild = function(child){
+  var index = this.children.indexOf(child);
+  if(index !== -1){
+    // remove the child
+    this.children.splice(index,1);
+  }else{
+    throw new Error("That node is not an immediate child of this tree");
+  }
+};
+
+/**
+* Testing
+*/
+
+var root1 = new Tree(1);
+var branch2 = root1.addChild(2);
+var branch3 = root1.addChild(3);
+var leaf4 = branch2.addChild(4);
+var leaf5 = branch2.addChild(5);
+var leaf6 = branch3.addChild(6);
+var leaf7 = branch3.addChild(7);
+
+var result = root1.astTraverse();
+console.log(result);  // Should print: [1, 2, 4, 2, 5, 2, 1, 3, 6, 3, 7, 3, 1]
