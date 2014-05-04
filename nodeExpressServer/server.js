f259d919a8c78155db985b27afb015185b9f2de9
Michael Tseng <matseng@gmail.com>
2014-05-03 19:03:30 -0700

    Completed nodeExpressServer hello world example

---
+++ nodeExpressServer/server.js
@@ -0,0 +1,16 @@
+/*
+* server.js
+* From command line, $ node server.js  or $ nodemon server.js
+* From browser address bar: http://localhost:3000/helloWorld
+*/
+
+var express = require('express');
+var app = express();
+
+app.get('/helloWorld', function(req, res){
+  res.send('Hello World');
+});
+
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
