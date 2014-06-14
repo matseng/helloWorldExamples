18f223387b393a1f720fe75839dcfd76dfedb145
Michael Tseng <matseng@gmail.com>
2014-06-13 17:22:22 -0700

    Added fibonacci done in forLoop style

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,7 +1,24 @@
 var fibonacci = function(n){
-  if (n <= 2)
+  if (n <= 1)
     return 1;
   return fibonacci(n-1) + fibonacci(n-2);
 }
 
-console.log(fibonacci(13));
+
+//0,1, 1,1, 2,2, 3,3, 4,5, 5,8, 6,13, 7,21, 8,34, 9,55
+
+function fibForLoop(n) {
+  var sumPrev = 1;
+  var sum = 1;
+  var sumTemp;
+  for(var i = 2; i <= n; i++) {
+    sumTemp = sum;
+    sum = sum + sumPrev;
+    sumPrev = sumTemp;
+  }
+  return sum;
+};
+
+var n = 9;
+console.log(fibonacci(n));
+console.log(fibForLoop(n));
