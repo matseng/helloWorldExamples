c72b2c0d42b3055ccc1049e2fd10e304d4feb300
Michael Tseng <matseng@gmail.com>
2014-06-14 08:52:54 -0700

    Added tileSwap problem

---
+++ tileSwap/app.js
@@ -0,0 +1,10 @@
+/*
+Given an ordered set of tiles, how many different permutations are there? 
+And more importantly, how many adjacent tile swaps did it take to make that permutation?
+Example: 
+  0 swaps: abc
+  1 swap: bac, acb
+  2 swaps: bca, cab
+  3 swaps: cba   
+  Summary: (permutation, # of swaps); (abc, 0), (bac, 1), (acb, 1), (bca, 2), (cab,2), (cba,3)
+*/
