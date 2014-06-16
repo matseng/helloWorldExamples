714e8562352f791c713b7bad4cc233fee711d79e
Michael Tseng <matseng@gmail.com>
2014-06-16 10:07:24 -0700

    Completed swapTile with pattern similar to N-queens for recursing over possible combos - toggle, recurse, toggle back

--- tileSwap/app.js
+++ tileSwap/app.js
@@ -8,3 +8,42 @@ Example:
   3 swaps: cba   
   Summary: (permutation, # of swaps); (abc, 0), (bac, 1), (acb, 1), (bca, 2), (cab,2), (cba,3)
 */
+
+Array.prototype.swap = function(i) {
+  var temp = this[i];
+  this[i] = this[i + 1];
+  this[i + 1] = temp;
+};
+
+function tileSwap(str) {
+  var result = {};
+  var arr = str.split("");
+  var strTemp;
+  var swapCounter = 0;
+  result[str] = 0;
+  var maxSwaps = (arr.length - 1) / 2 * (arr.length - 1 + 1);
+  // 4 3 2 1 = 10
+  // 5 4 3 2 1 = 15
+  // (n / 2) (n+1) = 2.5 + 6
+
+  var recur = function() {
+    for(var i = 0; i < arr.length - 1; i++) {
+      arr.swap(i);
+      strTemp = arr.join("");
+      swapCounter += 1;
+      if(swapCounter <= maxSwaps && !(strTemp in result) || (strTemp in result && swapCounter < result[strTemp])) {
+        console.log(strTemp, swapCounter);
+        result[strTemp] = swapCounter;
+        recur();
+      }
+      arr.swap(i);
+      swapCounter -= 1;
+    }
+  }
+  recur();
+  return result;
+};
+
+var result = tileSwap('abcd');
+console.log(result);
+console.log(Object.keys(result).length);
