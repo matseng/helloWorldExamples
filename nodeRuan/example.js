3f9495b2495c131f859deb38197526d364d4484a
Michael Tseng <matseng@gmail.com>
2014-06-21 17:03:05 -0700

    Added node server example from conversation with Ruan

---
+++ nodeRuan/example.js
@@ -0,0 +1,16 @@
+var http = require('http');
+fs = require('fs')
+
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  fs.readFile(__dirname + '/helloworld.txt', 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    res.end(data);
+  });
+});
+
+server.listen(1337, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:1337/');
