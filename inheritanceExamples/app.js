4e73e1f2bf2d0d6812ba47704ea0076f662aa473
Michael Tseng <matseng@gmail.com>
2014-07-10 17:38:30 -0700

    Added example problems / solutions with pseudoclassical inheritance

---
+++ inheritanceExamples/app.js
@@ -0,0 +1,104 @@
+
+
+//Rec from Hack Reactor's Facebook page: http://davidshariff.com/blog/javascript-inheritance-patterns/
+
+/**
+ * Point a child's prototype to a parent's prototype
+ **/
+
+if(!Object.create){
+  Object.create = function(o){
+    function F(){}
+    F.prototype=o;
+    return new F();
+  }
+}
+
+var extendObjProblem1 = function(childObj, parentObj) {
+  childObj.prototype = parentObj.prototype;  //Problem: change the child proto actually changes the parent
+};
+
+var extendObjProblem2 = function(childObj, parentObj) {
+  childObj.prototype = Object.create(parentObj.prototype);  //Problem: Won't inherit attributes (testing1)
+};
+
+var extendObj_v1 = function(childObj, parentObj) {
+  var tmpObj = function () {}
+  tmpObj.prototype = parentObj.prototype;
+  childObj.prototype = new tmpObj();  //Seems verbose, necessary?
+  childObj.prototype.constructor = childObj;
+};
+
+var extendObj_v2 = function(childObj, parentObj) {
+  childObj.prototype = new parentObj();  //my favorite solution thus far
+  childObj.prototype.constructor = childObj;
+};
+
+
+// base human object
+var Human = function() {
+  this.testing1 = 123;
+  name: '';
+  gender: '';
+};
+
+// inhertiable attributes / methods
+Human.prototype = {
+  testing2: 123,
+  planetOfBirth: 'Earth',  //I kept earth on the proto bc it should be true of all humans (but we can still change it later if we want to)
+  sayGender: function () {
+      console.log(this.name + ' says my gender is ' + this.gender);
+  },
+  sayPlanet: function () {
+      console.log(this.name + ' was born on ' + this.planetOfBirth);
+  },
+  sayTesting1: function() {
+    console.log("I'm testing: ", this.testing1);
+  },
+  sayTesting2: function() {
+    console.log("I'm testing: ", this.testing2);
+  }
+
+};
+
+// male
+var Male = function (name) {
+    this.gender = 'Male';
+    // this.name = 'David';  //bad example IMHO of using same name for every male
+    this.name = name;
+};
+// inherits human
+// extendObj(Male, Human);
+extendObj_v2(Male, Human);
+// extendObjProblem1(Male, Human);
+// extendObjProblem2(Male, Human);
+
+// female
+var Female = function (name) {
+    this.name = name;
+    this.gender = 'Female';
+};
+// inherits human
+extendObj_v2(Female, Human);
+
+// new instances
+var mike = new Male('Mike');
+var david = new Male('David');
+var jane = new Female('Jane');
+
+david.sayGender(); // David says my gender is Male
+jane.sayGender(); // Jane says my gender is Female
+mike.sayGender();
+
+// mike.planetOfBirth = 'Neptune';
+mike.sayPlanet();
+david.sayPlanet();
+Male.prototype.planetOfBirth = 'Mars';
+david.sayPlanet(); // David was born on Mars
+mike.sayPlanet();
+jane.sayPlanet(); // Problem if we use extendObjProblem: Jane was born on Mars
+
+// david.testing = 456;
+david.sayTesting1();
+david.sayTesting2();
+// jane.sayTesting();
