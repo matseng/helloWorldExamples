d39f0c97ad7de583fd53a3c4509de4df2fdbcf12
Michael Tseng <matseng@gmail.com>
2014-08-01 11:43:12 -0700

    Added karma and jasmine set up to template folder

---
+++ templateFolder/tests/appSpec.js
@@ -0,0 +1,9 @@
+/*
+
+describe('method / function name', function() {
+  it('should... describe what the method is intended to do', function() {
+    expect(myFunc('inputParam')).toBe(false);
+  })
+});
+
+*/
